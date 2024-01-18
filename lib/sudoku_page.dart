289c2868a093d1411e24d4466fa4e2e048d4f0bf
huggo-42 <victorhuggocontato42@gmail.com>
2024-01-18 18:34:42 -0300

    feat: implements "New game" and "Erase" buttons

--- lib/sudoku_page.dart
+++ lib/sudoku_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:sudoku/box_class.dart';
 import 'package:sudoku/utils/print_tool.dart';
@@ -88,9 +86,9 @@ class _SudokuPageState extends State<SudokuPage> {
     for (int i = 0; i < IndexHelper.cols[colIndex]!.length; ++i) {
       indexesToHighlight.add(IndexHelper.cols[colIndex]![i]);
     }
-    for (int i in indexesToHighlight) {
-      printTool("indexesToHighlight: $i");
-    }
+    // for (int i in indexesToHighlight) {
+    //   printTool("indexesToHighlight: $i");
+    // }
     for (int i in indexesToHighlight) {
       board.getBox(i).isHighlight = true;
     }
@@ -105,12 +103,139 @@ class _SudokuPageState extends State<SudokuPage> {
     setState(() {});
   }
 
+  void validateBoard() {
+    int solved = 0;
+    for (Box box in board.boxes.values) {
+      if (box.value == box.correctValue) ++solved;
+    }
+    String snackBarMessage;
+    if (solved == 81) {
+      snackBarMessage = "SOLVED";
+    } else {
+      snackBarMessage = "NOT SOLVED";
+    }
+    SnackBar snackBar = SnackBar(content: Text(snackBarMessage));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Create new game?'),
+          actions: <Widget>[
+            TextButton.icon(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: const Icon(
+                Icons.close,
+                size: 30,
+                color: blue400,
+              ),
+              label: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: blue400,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(blue100),
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () => Navigator.of(context).pop(),
+              icon: const Icon(
+                Icons.done,
+                size: 30,
+                color: blue400,
+              ),
+              label: const Text(
+                'OK',
+                style: TextStyle(
+                  color: blue400,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(blue100),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void newBoard() async {
+    bool confirm = await showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: const Text('Create new game?'),
+        actions: [
+          TextButton.icon(
+            onPressed: () => Navigator.pop(context, false),
+            icon: const Icon(
+              Icons.close,
+              size: 30,
+              color: blue400,
+            ),
+            label: const Text(
+              'Cancel',
+              style: TextStyle(
+                color: blue400,
+              ),
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(blue100),
+            ),
+          ),
+          TextButton.icon(
+            onPressed: () => Navigator.pop(context, true),
+            icon: const Icon(
+              Icons.done,
+              size: 30,
+              color: blue400,
+            ),
+            label: const Text(
+              'OK',
+              style: TextStyle(
+                color: blue400,
+              ),
+            ),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all<Color>(blue100),
+            ),
+          ),
+        ],
+      ),
+    );
+    if (confirm) {
+      genSudoku();
+      setState(() {});
+    }
+  }
+
   void handleKeyboardPress(int input) {
     Box box = board.getBox(selectedIndex);
     if (box.value != box.correctValue) {
       box.value = input;
-      int rowIndex = getRowIndex(selectedIndex);
-      boxSelected(rowIndex, getIndexInRow(rowIndex, selectedIndex));
+      // boxSelected(rowIndex, getIndexInRow(rowIndex, selectedIndex));
+      // int rowIndex = getRowIndex(selectedIndex);
+      // int indexInRow = getIndexInRow(rowIndex, selectedIndex);
+      // printTool("[DEBUG] input: $input, selectedIndex: $selectedIndex");
+      // printTool("[DEBUG] rowIndex: $rowIndex, indexInRow: $indexInRow");
+      int blockIndex = box.block;
+      int inBlockIndex = getIndexInBlock(box.block, box.boxIndex);
+      boxSelected(blockIndex, inBlockIndex);
+      printTool("[DEBUG] blockIndex: $blockIndex, inBlockIndex: $inBlockIndex");
+      setState(() {});
+    }
+  }
+
+  void eraseBox() {
+    Box box = board.getBox(selectedIndex);
+    if (box.value != box.correctValue) {
+      box.value = 0;
       setState(() {});
     }
   }
@@ -127,14 +252,18 @@ class _SudokuPageState extends State<SudokuPage> {
     printSudoku();
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Sudoku by huggo-42"),
+        title: const Text(
+          "Sudoku FOSS",
+          style: TextStyle(
+            color: blue400,
+          ),
+        ),
       ),
       body: SafeArea(
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(5),
               padding: const EdgeInsets.all(5),
               alignment: Alignment.center,
               child: GridView.builder(
@@ -193,6 +322,9 @@ class _SudokuPageState extends State<SudokuPage> {
                         }
                         if (box.value == selectedValue && selectedValue != 0) {
                           color = blue200;
+                        }
+                        if (box.value != box.correctValue) {
+                          colorText = Colors.red;
                         }
 
                         List<int> shouldHaveBottomRight = [0, 1, 3, 4];
@@ -258,6 +390,7 @@ class _SudokuPageState extends State<SudokuPage> {
                                       '',
                               style: TextStyle(
                                 color: colorText,
+                                fontSize: 20,
                               ),
                             ),
                           ),
@@ -275,14 +408,25 @@ class _SudokuPageState extends State<SudokuPage> {
                 itemCount: 9,
                 shrinkWrap: true,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 9,
-                  childAspectRatio: 1,
-                  crossAxisSpacing: 2,
+                  // crossAxisCount: 9,
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 4,
+                  mainAxisSpacing: 4,
+                  childAspectRatio: 2,
                 ),
                 physics: const ScrollPhysics(),
                 itemBuilder: (BuildContext buildContext, int idx) {
                   return TextButton(
                     onPressed: () => handleKeyboardPress(idx + 1),
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(8),
+                      backgroundColor: blue100,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(0),
+                        ),
+                      ),
+                    ),
                     child: Text(
                       "${idx + 1}",
                       style: const TextStyle(
@@ -295,15 +439,62 @@ class _SudokuPageState extends State<SudokuPage> {
                 },
               ),
             ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton.icon(
+                  onPressed: () => newBoard(),
+                  icon: const Icon(
+                    Icons.add,
+                    size: 30,
+                    color: blue400,
+                  ),
+                  label: const Text(
+                    'New game',
+                    style: TextStyle(
+                      color: blue400,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(blue100),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Column(
+                  children: [
+                    IconButton(
+                      onPressed: () => eraseBox(),
+                      icon: const Icon(
+                        Icons.clear,
+                        size: 30,
+                        color: blue400,
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(blue100),
+                      ),
+                    ),
+                    const Text(
+                      'Erase',
+                      style: TextStyle(
+                        color: blue400,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          board.printBoardState();
-        },
-        child: const Icon(Icons.supervisor_account),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     board.printBoardState();
+      //   },
+      //   child: const Icon(Icons.adb),
+      // ),
     );
   }
 }
@@ -357,5 +548,11 @@ int getIndexInRow(int rowIndex, int boxIndex, {bool debug = false}) {
     printTool("rowIndex: $rowIndex, boxIndex: $boxIndex");
     printTool("rowIndex: $rowIndex, boxIndex: $boxIndex");
   }
+  return a;
+}
+
+int getIndexInBlock(int blockIndex, int boxIndex) {
+  int a = -1;
+  a = IndexHelper.blocks[blockIndex]!.indexOf(boxIndex);
   return a;
 }
